6e74b85b6ee8082dd19427e625daddc19fb02520
Douglas Lins <douglas_lins11@hotmail.com>
2020-08-02 22:10:22 -0300

    commit inicial

---
+++ src/main/java/com/lins/linsmoneyapi/LinsmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.lins.linsmoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinsmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinsmoneyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lins/linsmoneyapi/LinsmoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lins.linsmoneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinsmoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
